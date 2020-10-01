a12fc82c24d229da6a21731195725231c2967ce9
Tien Duong <tienduong@autonomous.nyc>
2020-10-01 15:04:18 +0700

    Update wasm file and domain config

--- env/common.env.js
+++ env/common.env.js
@@ -1,6 +1,6 @@
 module.exports = {
   DEFAULT_CHAIN_URL_MAINNET: 'https://mainnet.incognito.org/fullnode',
-  DEFAULT_CHAIN_URL_TESTNET: 'https://test-node.incognito.org',
+  DEFAULT_CHAIN_URL_TESTNET: 'https://testnet.incognito.org/fullnode',
   DEFAULT_API_URL_MAINNET: 'https://api.incognito.org',
-  DEFAULT_API_URL_TESTNET: 'https://test-api2.incognito.org',
+  DEFAULT_API_URL_TESTNET: 'https://staging-api-service.incognito.org',
 };
