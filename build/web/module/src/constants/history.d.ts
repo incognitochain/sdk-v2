872ec3e50d8cd4b6688e86b335c8854311cc491b
jayce <phathoang@incognito.org>
2020-11-30 15:20:23 +0700

    Add history constant

---
+++ build/web/module/src/constants/history.d.ts
@@ -0,0 +1,65 @@
+export declare const HISTORY: {
+    TYPE: {
+        SHIELD: number;
+        UNSHIELD: number;
+        SEND: number;
+        RECEIVE: number;
+        PROVIDE: number;
+    };
+    STATUS_TEXT: {
+        SUCCESS: string;
+        FAILED: string;
+        PENDING: string;
+        EXPIRED: string;
+    };
+    META_DATA_TYPE: {
+        44: string;
+        63: string;
+        90: string;
+        93: string;
+        27: string;
+        127: string;
+        240: string;
+    };
+    STATUS_CODE: {
+        PENDING: number;
+    };
+    STATUS_CODE_SHIELD_DECENTRALIZED: {
+        PENDING: number;
+        PROCESSING: number[];
+        COMPLETE: number;
+        TIMED_OUT: number;
+        RETRYING: number;
+    };
+    STATUS_CODE_SHIELD_CENTRALIZED: {
+        PENDING: number;
+        PROCESSING: number[];
+        COMPLETE: number[];
+        TIMED_OUT: number[];
+    };
+    STATUS_CODE_UNSHIELD_DECENTRALIZED: {
+        PROCESSING: number[];
+        FAILED: number[];
+        COMPLETE: number;
+        RETRYING: number[];
+        TIMED_OUT: number;
+    };
+    STATUS_CODE_UNSHIELD_CENTRALIZED: {
+        PENDING: number;
+        PROCESSING: number[];
+        COMPLETE: number;
+        RETRYING: number;
+        TIMED_OUT: number;
+    };
+    TYPE_HISTORY_RECEIVE: {
+        41: string;
+        45: string;
+        81: string;
+        94: string;
+        95: string;
+        96: string;
+        25: string;
+        92: string;
+    };
+};
+//# sourceMappingURL=history.d.ts.map
