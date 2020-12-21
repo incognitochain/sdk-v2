2bfc6d87261a50f4c5cae2af8e3d91333ec5a662
jayce <phathoang@incognito.org>
2020-12-21 14:50:05 +0700

    Check payment addr

--- src/services/key/keyWallet.ts
+++ src/services/key/keyWallet.ts
@@ -17,17 +17,41 @@ import { ENCODE_VERSION, ED25519_KEY_SIZE } from '@src/constants/constants';
 import PrivateKeyModel from '@src/models/key//privateKey';
 import PaymentAddressKeyModel from '@src/models/key/paymentAddress';
 import ViewingKeyModel from '@src/models/key//viewingKey';
-import { getBackupData as getBackupDataKeySet, restoreKeySetFromBackupData } from '@src/services/key/accountKeySet';
+import {
+  getBackupData as getBackupDataKeySet,
+  restoreKeySetFromBackupData,
+} from '@src/services/key/accountKeySet';
 import CryptoJS from 'crypto-js';
-import { wordArrayToByteArray, byteArrayToWordArray } from '@src/services/key/utils';
+import {
+  wordArrayToByteArray,
+  byteArrayToWordArray,
+} from '@src/services/key/utils';
 import KeyWalletModel from '@src/models/key/keyWallet';
 import Validator from '@src/utils/validator';
-import { getKeyBytes, extractPaymentAddressKey, extractPrivateKey, extractViewingKey } from '@src/utils/key';
+import {
+  getKeyBytes,
+  extractPaymentAddressKey,
+  extractPrivateKey,
+  extractViewingKey,
+} from '@src/utils/key';
+import KEYS from '@src/constants/keys';
 
-type AllKeyModelType = PrivateKeyModel | PaymentAddressKeyModel | ViewingKeyModel;
-type KeyTypeString = 'PRIVATE_KEY' | 'PAYMENT_ADDRESS' | 'PUBLIC_KEY' | 'VIEWING_KEY';
+type AllKeyModelType =
+  | PrivateKeyModel
+  | PaymentAddressKeyModel
+  | ViewingKeyModel;
+type KeyTypeString =
+  | 'PRIVATE_KEY'
+  | 'PAYMENT_ADDRESS'
+  | 'PUBLIC_KEY'
+  | 'VIEWING_KEY';
 
-export function serializeKey(key : AllKeyModelType, depth: KeyWalletDepth, childNumber: KeyWalletChildNumber, chainCode: KeyWalletChainCode) {
+export function serializeKey(
+  key: AllKeyModelType,
+  depth: KeyWalletDepth,
+  childNumber: KeyWalletChildNumber,
+  chainCode: KeyWalletChainCode
+) {
   new Validator('key', key).required();
   new Validator('depth', depth).required().number();
   new Validator('childNumber', childNumber).required();
@@ -55,7 +79,6 @@ export function serializeKey(key : AllKeyModelType, depth: KeyWalletDepth, child
     keyBytes.set([(<PrivateKeyModel>key).privateKeyBytes.length], offset);
     offset += 1;
     keyBytes.set((<PrivateKeyModel>key).privateKeyBytes, offset);
-
   } else if (keyType === PaymentAddressType) {
     keyBytes = new Uint8Array(PaymentAddrSerializeSize);
     let offset = 0;
@@ -67,10 +90,12 @@ export function serializeKey(key : AllKeyModelType, depth: KeyWalletDepth, child
     keyBytes.set((<PaymentAddressKeyModel>key).publicKeyBytes, offset);
     offset += ED25519_KEY_SIZE;
 
-    keyBytes.set([(<PaymentAddressKeyModel>key).transmissionKeyBytes.length], offset);
+    keyBytes.set(
+      [(<PaymentAddressKeyModel>key).transmissionKeyBytes.length],
+      offset
+    );
     offset += 1;
     keyBytes.set((<PaymentAddressKeyModel>key).transmissionKeyBytes, offset);
-
   } else if (keyType === ViewingKeyType) {
     keyBytes = new Uint8Array(ReadonlyKeySerializeSize);
     let offset = 0;
@@ -112,15 +137,17 @@ function deserializeKeyValidate(bytes: KeyBytes) {
   }
 }
 
-export function deserializePrivateKeyBytes(bytes: KeyBytes) : {
-  depth: KeyWalletDepth,
-  childNumber: KeyWalletChildNumber,
-  chainCode: KeyWalletChainCode,
-  privateKey: PrivateKeyModel
+export function deserializePrivateKeyBytes(
+  bytes: KeyBytes
+): {
+  depth: KeyWalletDepth;
+  childNumber: KeyWalletChildNumber;
+  chainCode: KeyWalletChainCode;
+  privateKey: PrivateKeyModel;
 } {
   new Validator('bytes', bytes).required();
 
-  const { depth, childNumber, chainCode, keyLength} = extractPrivateKey(bytes);
+  const { depth, childNumber, chainCode, keyLength } = extractPrivateKey(bytes);
 
   const privateKey = new PrivateKeyModel(bytes.slice(39, 39 + keyLength));
 
@@ -130,16 +157,20 @@ export function deserializePrivateKeyBytes(bytes: KeyBytes) : {
     depth,
     childNumber,
     chainCode,
-    privateKey
+    privateKey,
   };
 }
 
-export function deserializePaymentAddressKeyBytes(bytes: KeyBytes) : PaymentAddressKeyModel{
+export function deserializePaymentAddressKeyBytes(
+  bytes: KeyBytes
+): PaymentAddressKeyModel {
   new Validator('bytes', bytes).required();
 
   const paymentAddress = new PaymentAddressKeyModel();
 
-  const { publicKeyBytes, transmissionKeyBytes } = extractPaymentAddressKey(bytes);
+  const { publicKeyBytes, transmissionKeyBytes } = extractPaymentAddressKey(
+    bytes
+  );
 
   paymentAddress.publicKeyBytes = publicKeyBytes;
   paymentAddress.transmissionKeyBytes = transmissionKeyBytes;
@@ -149,7 +180,7 @@ export function deserializePaymentAddressKeyBytes(bytes: KeyBytes) : PaymentAddr
   return paymentAddress;
 }
 
-export function deserializeViewingKeyBytes(bytes: KeyBytes) : ViewingKeyModel{
+export function deserializeViewingKeyBytes(bytes: KeyBytes): ViewingKeyModel {
   new Validator('bytes', bytes).required();
 
   const viewingKey = new ViewingKeyModel();
@@ -164,18 +195,23 @@ export function deserializeViewingKeyBytes(bytes: KeyBytes) : ViewingKeyModel{
   return viewingKey;
 }
 
-export function deserializePublicKeyBytes(bytes: KeyBytes) : KeyBytes {
+export function deserializePublicKeyBytes(bytes: KeyBytes): KeyBytes {
   new Validator('bytes', bytes).required();
 
   const publicKeyLength = bytes[1];
-  const publicKeyBytes =  bytes.slice(2, 2 + publicKeyLength);
+  const publicKeyBytes = bytes.slice(2, 2 + publicKeyLength);
 
   deserializeKeyValidate(bytes);
 
   return publicKeyBytes;
 }
 
-export function base58CheckSerialize(key: AllKeyModelType, depth: KeyWalletDepth, childNumber: KeyWalletChildNumber, chainCode: KeyWalletChainCode) {
+export function base58CheckSerialize(
+  key: AllKeyModelType,
+  depth: KeyWalletDepth,
+  childNumber: KeyWalletChildNumber,
+  chainCode: KeyWalletChainCode
+) {
   new Validator('key', key).required();
   new Validator('depth', depth).required();
   new Validator('childNumber', childNumber).required();
@@ -193,7 +229,7 @@ export function base58CheckDeserialize(keyStr: string) {
   let type: KeyTypeString;
   let key;
 
-  switch(keyType) {
+  switch (keyType) {
     case PriKeyType:
       type = 'PRIVATE_KEY';
       key = deserializePrivateKeyBytes(keyBytes);
@@ -215,13 +251,19 @@ export function base58CheckDeserialize(keyStr: string) {
   return { type, key };
 }
 
-export function getIntermediary(childIndex: number, keyWalletChainCode: KeyWalletChainCode) {
+export function getIntermediary(
+  childIndex: number,
+  keyWalletChainCode: KeyWalletChainCode
+) {
   new Validator('childIndex', childIndex).required();
   new Validator('keyWalletChainCode', keyWalletChainCode).required();
 
-  let childIndexBytes = (new bn(childIndex)).toArray();
+  let childIndexBytes = new bn(childIndex).toArray();
   // HmacSHA512(data, key)
-  let hmac = CryptoJS.HmacSHA512(CryptoJS.enc.Base64.stringify(byteArrayToWordArray(keyWalletChainCode)), byteArrayToWordArray(Uint8Array.from(childIndexBytes)));
+  let hmac = CryptoJS.HmacSHA512(
+    CryptoJS.enc.Base64.stringify(byteArrayToWordArray(keyWalletChainCode)),
+    byteArrayToWordArray(Uint8Array.from(childIndexBytes))
+  );
   let intermediary = wordArrayToByteArray(hmac);
   return intermediary;
 }
@@ -233,7 +275,7 @@ export function getBackupData(keyWallet: KeyWalletModel) {
     chainCode: Array.from(keyWallet.chainCode),
     childNumber: Array.from(keyWallet.childNumber),
     depth: keyWallet.depth,
-    keySet: getBackupDataKeySet(keyWallet.keySet)
+    keySet: getBackupDataKeySet(keyWallet.keySet),
   };
 
   return data;
@@ -252,3 +294,18 @@ export function restoreKeyWalletFromBackupData(data: any) {
 
   return keyWallet;
 }
+
+export const checkPaymentAddress = (paymentAddr: string) => {
+  new Validator('data', paymentAddr).required();
+  let result = false;
+  try {
+    const { keyBytes } = getKeyBytes(paymentAddr);
+    const key = deserializePaymentAddressKeyBytes(keyBytes);
+    result =
+      key.publicKeyBytes.length === KEYS.PUBLIC_KEY_BYTES_LENGTH &&
+      key.transmissionKeyBytes.length === KEYS.PUBLIC_KEY_BYTES_LENGTH;
+  } catch (error) {
+    result = false;
+  }
+  return result;
+};
